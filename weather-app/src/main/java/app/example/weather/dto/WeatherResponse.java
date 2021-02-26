e7829a56a4ba77928575474e257a952a87d1a13a
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-02-26 14:59:44 +0100

    Add Weather Controller

---
+++ weather-app/src/main/java/app/example/weather/dto/WeatherResponse.java
@@ -0,0 +1,32 @@
+package app.example.weather.dto;
+
+import com.raynigon.unit_api.core.annotation.QuantityShape;
+import com.raynigon.unit_api.core.units.si.dimensionless.Percent;
+import com.raynigon.unit_api.core.units.si.temperature.Celsius;
+import com.raynigon.unit_api.jackson.annotation.JsonUnit;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Dimensionless;
+import javax.measure.quantity.Temperature;
+
+public class WeatherResponse {
+
+    @JsonUnit(value = Celsius.class, shape = QuantityShape.STRING)
+    private final Quantity<Temperature> temperature;
+
+    @JsonUnit(value = Percent.class, shape = QuantityShape.STRING)
+    private final Quantity<Dimensionless> humidity;
+
+    public WeatherResponse(Quantity<Temperature> temperature, Quantity<Dimensionless> humidity) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public Quantity<Temperature> getTemperature() {
+        return temperature;
+    }
+
+    public Quantity<Dimensionless> getHumidity() {
+        return humidity;
+    }
+}
--- weather-app/src/test/java/app/example/weather/WeatherAppApplicationTests.java
+++ weather-app/src/test/java/app/example/weather/WeatherAppApplicationTests.java
@@ -1,13 +1,38 @@
 package app.example.weather;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WeatherAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    private RestTemplate restTemplate;
+
+    @BeforeEach
+    void setup() {
+        restTemplate = new RestTemplate();
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void get_api_weather() {
+        Map<String, Object> result = restTemplate.getForObject("http://localhost:" + port + "/api/weather", Map.class);
+        assertEquals(Map.of(
+                "temperature", "30 \u2103",
+                "humidity", "45 %"
+        ), result);
+    }
 
 }
