0c77a53c1a9e1e4d6cefc92a70b005c74f2dcdd6
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2022-12-15 11:45:58 +0100

    Fix Spring Boot 3 Update

--- weather-app/src/test/java/app/example/weather/WeatherAppApplicationTests.java
+++ weather-app/src/test/java/app/example/weather/WeatherAppApplicationTests.java
@@ -3,7 +3,7 @@ package app.example.weather;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
